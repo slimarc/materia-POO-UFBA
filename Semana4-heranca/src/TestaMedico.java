e2849fd8a9e46aae888a5152980bcc07f249ee28
Ricardo Lima <sricardolimaa@gmail.com>
2022-09-30 23:29:30 -0300

    testando classe medico

--- Semana4-heranca/src/TestaMedico.java
+++ Semana4-heranca/src/TestaMedico.java
@@ -4,22 +4,22 @@ public class TestaMedico {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		Medico medico =  new Medico();
-		
-		System.out.println("========FORMULÁRIO========");
-		System.out.println("\nInsira dos dados do funcionário: ");
-		
-		
-		medico.getCodigo();
-		
-		
-		
-		
+
+		medico.setCodigo(1323);
+		medico.setNome("Ricardo");
+		medico.setSalario(27.871);
+		medico.setDataContratacao("12/08/2015");
+		medico.setAnos(12);
+		medico.setCrm(1541);
+		medico.setEspecialidade("Otorrino");	
+
 		System.out.println("============DADOS FUNCIONÁRIO================");
 		System.out.println("Cod func: " + medico.getCodigo() + "\nNome func: " + medico.getNome()
 							+ "\nSalário: " + medico.getSalario() + "\nData de contratação: " + medico.getDataContratacao()
 							+ "\nTempo de profissão: " + medico.getAnos() + "\nEspecialidade: " + medico.getEspecialidade()
 							+ "\nCRM: " + medico.getCrm());
 		
+	
 	}
 
 }
